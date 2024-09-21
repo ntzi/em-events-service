80a5520c64f1b0a96672c6b37058eb06efb2fb17
Nikos Tziralis <ntziralis@gmail.com>
2024-09-21 19:17:53 +0300

    clean up

--- src/api/routes/events.ts
+++ src/api/routes/events.ts
@@ -2,17 +2,9 @@ import { Router } from 'express';
 
 import { getArtistIds } from '../controllers/events.js';
 import { asyncHandler } from '../middleware/asyncHandler.js';
-import isAuthenticated from '../middleware/auth/authenticated.js';
-import { validate } from '../validators/requestValidator.js';
-import { getRelevantSchema } from '../validators/schemas/request/events.js';
 
 const router = Router();
 
-router.get(
-	'/v1/events/:eventId/artists',
-	// isAuthenticated,
-	asyncHandler(getArtistIds)
-);
+router.get('/v1/events/:eventId/artists', asyncHandler(getArtistIds));
 
 export default router;
-
--- src/api/validators/schemas/response/events.ts
+++
@@ -1,3 +0,0 @@
-const getRelevantFansResData = ['id', 'name'];
-
-export { getRelevantFansResData };
