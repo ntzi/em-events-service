e197132f5ec52aef88262b27912a2c6a12bd4927
Nikos Tziralis <ntziralis@gmail.com>
2024-09-22 17:30:21 +0300

    Add tables and update endpoint

--- src/api/controllers/events.ts
+++ src/api/controllers/events.ts
@@ -1,9 +1,14 @@
+import { Artist } from '../models/artist.js';
+
 const getArtistIds = async (req, res) => {
 	const { eventId } = req.params;
 
-	const resData = [{ id: '1' }, { id: '2' }];
+	const artists = await Artist.createQueryBuilder('artist')
+		.innerJoin('artist.events', 'event')
+		.where('event.id = :eventId', { eventId })
+		.getMany();
 
-	return res.ok(resData);
+	return res.ok(artists);
 };
 
 export { getArtistIds };
