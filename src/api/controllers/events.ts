a0bdfe3e2671e4fff1eca015b4894f60068bb35d
Nikos Tziralis <ntziralis@gmail.com>
2024-09-22 19:52:02 +0300

    skip db sync, clean up

--- src/api/controllers/events.ts
+++ src/api/controllers/events.ts
@@ -6,9 +6,13 @@ const getArtistIds = async (req, res) => {
 	const artists = await Artist.createQueryBuilder('artist')
 		.innerJoin('artist.events', 'event')
 		.where('event.id = :eventId', { eventId })
+		.select(['artist.id'])
+		.cache(true)
 		.getMany();
 
-	return res.ok(artists);
+	const artistIds = artists.map((artist) => artist.id);
+
+	return res.ok(artistIds);
 };
 
 export { getArtistIds };
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import app from './api/loaders/appLoader.js';
 import { postgresLoad } from './api/loaders/dbLoader.js';
-import { ResponseI } from './api/types/responses/responsesTypes.js';
 import config from './config/config.js';
 
 const {
@@ -10,12 +9,7 @@ const {
 await postgresLoad();
 
 app.listen(port, () => {
-	console.log(`Gateway-service is listening on port ${port}`);
-});
-
-app.get('/', async (_req, res: ResponseI) => {
-	console.log('Events-Service is running...');
-	return res.ok('Events-Service is running...');
+	console.log(`Events-service is listening on port ${port}`);
 });
 
 export default app;
